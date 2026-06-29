02f2effc7c0ea15a57883e1142938647dc463973
Tyler Swenson <tylerjswenson262@gmail.com>
2026-06-29 06:19:15 -0500

    feat(backend): Added Dockerfile and .dockerignore to run backend.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -35,7 +35,8 @@ fastify.get('/ping', async(request, reply) => {
 // Run the server
 const run = async () => {
     try {
-        await fastify.listen({ port: 3000, host: '127.0.0.1' });
+        const port = Number(process.env.PORT || 3000);
+        await fastify.listen({ port: port, host: '0.0.0.0' });
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
