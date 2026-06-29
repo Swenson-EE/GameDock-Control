17b8bc3b82bc50a7fc7d68c4af740d5103b4c6a4
Tyler Swenson <tylerjswenson262@gmail.com>
2026-06-29 08:05:39 -0500

    feat(frontend): Added development and production targets for build

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true // Needed for Docker tracking and port mapping
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+
+  return {
+    plugins: [react()],
+
+    server: {
+      port: env.VITE_PORT ? Number(env.VITE_PORT) : 5173,
+      host: env.VITE_HOST || true
+    }
   }
-})
+});
+
